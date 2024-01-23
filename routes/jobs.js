185ee3c2c2bb120a9605b9974530d14bcfdb4f90
Igor <malcanoficial@gmail.com>
2024-01-23 17:44:57 -0300

    first commit

---
+++ routes/jobs.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../models/Job');
+const { json } = require('body-parser');
+
+
+//rota de teste
+router.get('/test', (req, res) => {
+    res.send('deu certo a rota...');
+})
+
+// detalhe da vaga 
+router.get('/view/:id', (req, res) => Job.findOne({
+    where: { id: req.params.id }
+})
+    .then(job => {
+        res.render('view', {
+            job
+        });
+    })
+    .catch(err => console.log(err)));
+// form da rota de view 
+router.get('/add', (req, res) => {
+    res.render('add');
+})
+
+//add job via post 
+router.post('/add', (req, res) => {
+    let { title, salary, company, description, email, new_job } = req.body;
+
+    // insert 
+
+    Job.create({
+        title,
+        description,
+        salary,
+        company,
+        email,
+        new_job
+    })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err));
+});
+
+module.exports = router
